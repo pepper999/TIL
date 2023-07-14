d7a8da0a718f210e9d92f9d39d38d66e3d973831
정지헌 <hun01414@naver.com>
2023-07-14 17:42:42 +0900

    3일차 찐찐막

--- 00_startcamp/BAEKJOON/test.py
+++ 00_startcamp/BAEKJOON/test.py
@@ -1,12 +1,6 @@
-N = int(input())
-if N == 1:
-    print(0)
-for i in range(1, N+1):
-    num = i + sum(list(map(int, str(i))))
-    if num == N:
-        print(i)
-        break
-if num != N:
-        print(0)
+import random
+students = ['강성은','강주원','고유한','김연빈','김지수','황성주','김해인','문지호','박주헌','이수빈','배준형','손종민','신현기','옥세훈','이승집','이준우','이흔오','장현욱','정지헌','정창휘','조혜원','최용훈','최재성','탁윤희']
+random.shuffle(students)
+print(students)
     
     
