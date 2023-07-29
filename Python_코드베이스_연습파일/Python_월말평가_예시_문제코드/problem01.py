425feb093589a28868688e09179aa4713d29650c
정지헌 <hun01414@naver.com>
2023-07-29 12:57:58 +0900

    시험문제예제

---
+++ Python_코드베이스_연습파일/Python_월말평가_예시_문제코드/problem01.py
@@ -0,0 +1,12 @@
+# 파이썬 내장함수 min 사용 금지
+def min_score(scores):
+    pass
+    # 여기에 코드를 작성합니다.
+
+
+if __name__ == '__main__':
+    # 아래의 코드는 수정하지 않습니다.
+    scores = [30, 60, 90, 70]
+    print(min_score(scores)) # 30
+    # 아래 부터 추가 예제 코드 작성 가능합니다.
+    
---
+++ Python_코드베이스_연습파일/Python_월말평가_예시_문제코드/problem03.py
@@ -0,0 +1,21 @@
+def is_user_data_valid(user_data):
+    pass
+    # 여기에 코드를 작성합니다.
+
+
+if __name__ == '__main__':
+    # 아래의 코드는 수정하지 않습니다.
+    user_data1 = {
+        'id': '',
+        'password': '1q2w3e4r',
+    }
+    print(is_user_data_valid(user_data1)) 
+    # False 
+
+    user_data2 = {
+        'id': 'jungssafy',
+        'password': '1q2w3e4r',
+    }
+    print(is_user_data_valid(user_data2)) 
+    # True
+    # 아래 부터 추가 예제 코드 작성 가능합니다.
