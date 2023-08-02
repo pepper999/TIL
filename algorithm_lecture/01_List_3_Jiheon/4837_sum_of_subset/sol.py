60c3bcfc4ab951557093d3c808a08eee78e1f1a6
정지헌 <hun01414@naver.com>
2023-08-02 14:11:47 +0900

    비트추

--- algorithm_lecture/01_List_3_Jiheon/4837_sum_of_subset/sol.py
+++ algorithm_lecture/01_List_3_Jiheon/4837_sum_of_subset/sol.py
@@ -1,18 +1,36 @@
 import sys
 sys.stdin = open('input.txt')
 
+# T = int(input())
+
+# subset_list = [[]]
+# for i in range(1, 13):
+#         size = len(subset_list)
+#         for j in range(size):
+#             subset_list.append(subset_list[j]+[i])
+
+# for t in range(T):
+#     N, K = map(int, input().split())
+#     cnt = 0
+#     for i in range(len(subset_list)):
+#         if len(subset_list[i]) == N and sum(subset_list[i]) == K:
+#             cnt += 1
+#     print(f'#{t+1}', cnt)
+
 T = int(input())
 
-subset_list = [[]]
-for i in range(1, 13):
-        size = len(subset_list)
-        for j in range(size):
-            subset_list.append(subset_list[j]+[i])
+list = [i for i in range(1, 13)]
 
 for t in range(T):
     N, K = map(int, input().split())
+
+    n = len(list)
     cnt = 0
-    for i in range(len(subset_list)):
-        if len(subset_list[i]) == N and sum(subset_list[i]) == K:
+    for i in range(1 << n):
+        subset = []
+        for j in range(n):
+            if i & (1 << j):
+                subset.append(list[j])
+        if len(subset) == N and sum(subset) == K:
             cnt += 1
     print(f'#{t+1}', cnt)
