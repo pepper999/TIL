f1d31d427b248d18945bdedf402ccf73b78af2f5
정지헌 <hun01414@naver.com>
2023-08-03 11:46:34 +0900

    ohbignum

---
+++ algorithm_lecture/01_List_4_Jiheon/4843_specialsort/sol.py
@@ -0,0 +1,41 @@
+import sys
+sys.stdin = open('input.txt')
+
+# T = int(input())
+# for t in range(T):
+#     N = int(input())
+#     num = list(map(int, input().split()))
+#     num.sort()
+#     special_sort = [0] * N
+#     mid = int(N/2)
+#     for i in range(1, mid+1):
+#         special_sort[i*2 - 1] = num[i - 1]
+#     for i in range(mid):
+#         special_sort[i*2] = num[-i-1]
+#     print(f'#{t+1}', end = ' ')
+#     for i in range(N):
+#         print(special_sort[i], end = ' ')
+#         print()
+
+T = int(input())
+for t in range(T):
+
+    N = int(input())
+    num = list(map(int, input().split()))
+    num.sort()
+    rlt = []
+    for i in range(5):
+            rlt.append(num.pop(-1))
+            rlt.append(num.pop(0))
+    
+    print(f'#{t+1}', *rlt)
+
+
+# def select(list, n):
+#     for i in range(n):
+#         minindex = i
+#         for j in range(i+1, len(list)):
+#             if list[minindex] > list[j]:
+#                 minindex = j
+#         list[i], list[minindex] = list[minindex], list[i]
+#         return list[n-1]
