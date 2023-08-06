23a674960efb15427016a81253645d1a3c307aee
정지헌 <hun01414@naver.com>
2023-08-06 23:10:50 +0900

    0806공부

--- algorithm_lecture/01_List_3_Jiheon/4836_color/sol.py
+++ algorithm_lecture/01_List_3_Jiheon/4836_color/sol.py
@@ -1,28 +1,55 @@
 import sys
 sys.stdin = open('input.txt')
+import pprint
+
+# T = int(input())
+# for t in range(T):
+#     N = int(input())
+# # 도화지로 쓸 paper 2차원 리스트 형성
+#     paper = [[0]*10 for i in range(10)]
+#     for i in range(N):
+#         ls = list(map(int, input().split()))
+#         # 입력 리스트의 마지막 값을 확인하여 색칠할 색깔 확인
+#         if ls[-1] == 1:
+#             # 주어진 x좌표, y좌표를 들고옴
+#             for j in range(ls[0], ls[2]+1):
+#                 for k in range(ls[1], ls[3]+1):
+#                     # 종이가 안칠해져있다면(0 -> 초기값)
+#                     if paper[j][k] == 0:
+#                         # 1을 칠함
+#                         paper[j][k] = 1
+#                     # 종이에 다른색이 칠해져 있다면 섞인색(3)이 칠해지도록 함
+#                     elif paper[j][k] == 2:
+#                         paper[j][k] = 3
+#         elif ls[-1] == 2:
+#             for j in range(ls[0], ls[2]+1):
+#                 for k in range(ls[1], ls[3]+1):
+#                     if paper[j][k] == 0:
+#                         paper[j][k] = 2
+#                     elif paper[j][k] == 1:
+#                         paper[j][k] = 3
+#     cnt = 0
+#     for i in range(10):
+#         cnt += paper[i].count(3)
+#     print(f'#{t+1}', cnt)
 
 T = int(input())
 for t in range(T):
     N = int(input())
-# 도화지로 쓸 paper 2차원 리스트 형성
-    paper = [[0]*10 for i in range(10)]
+    paper = [[0] * 10 for _ in range(10)]
     for i in range(N):
-        ls = list(map(int, input().split()))
-        # 입력 리스트의 마지막 값을 확인하여 색칠할 색깔 확인
-        if ls[-1] == 1:
-            # 주어진 x좌표, y좌표를 들고옴
-            for j in range(ls[0], ls[2]+1):
-                for k in range(ls[1], ls[3]+1):
-                    # 종이가 안칠해져있다면(0 -> 초기값)
+        query = list(map(int, input().split()))
+
+        if query[-1] == 1:
+            for j in range(query[0], query[2]+1):
+                for k in range(query[1], query[3]+1):
                     if paper[j][k] == 0:
-                        # 1을 칠함
                         paper[j][k] = 1
-                    # 종이에 다른색이 칠해져 있다면 섞인색(3)이 칠해지도록 함
                     elif paper[j][k] == 2:
                         paper[j][k] = 3
-        elif ls[-1] == 2:
-            for j in range(ls[0], ls[2]+1):
-                for k in range(ls[1], ls[3]+1):
+        else:
+            for j in range(query[0], query[2]+1):
+                for k in range(query[1], query[3]+1):
                     if paper[j][k] == 0:
                         paper[j][k] = 2
                     elif paper[j][k] == 1:
@@ -30,4 +57,4 @@ for t in range(T):
     cnt = 0
     for i in range(10):
         cnt += paper[i].count(3)
-    print(f'#{t+1}', cnt)
+    print(cnt)
--- algorithm_lecture/extra_01/2001_kill_fly/sol.py
+++ algorithm_lecture/extra_01/2001_kill_fly/sol.py
@@ -1,18 +1,36 @@
 import sys
 sys.stdin = open('input.txt')
 
+# T = int(input())
+# for t in range(T):
+#     N, M = map(int, input().split())
+#     fly = []
+#     for i in range(N):
+#         fly.append(list(map(int, input().split())))
+#     sum_max = 0
+#     for i in range(N-M+1):
+#         for j in range(N-M+1):
+#             temp_sum = 0
+#             for k in range(i, i+M):
+#                 temp_sum += sum(fly[k][j:j+M])
+#             if temp_sum > sum_max:
+#                 sum_max = temp_sum
+#     print(f'#{t+1}', sum_max)
+
 T = int(input())
 for t in range(T):
     N, M = map(int, input().split())
     fly = []
     for i in range(N):
         fly.append(list(map(int, input().split())))
-    sum_max = 0
+        temp_max = 0
     for i in range(N-M+1):
         for j in range(N-M+1):
-            temp_sum = 0
-            for k in range(i, i+M):
-                temp_sum += sum(fly[k][j:j+M])
-            if temp_sum > sum_max:
-                sum_max = temp_sum
-    print(f'#{t+1}', sum_max)
+            temp_cnt = 0
+            for k in range(M):
+                for l in range(M):
+                    temp_cnt += fly[i+k][j+l]
+            if temp_cnt > temp_max:
+                temp_max = temp_cnt
+    print(temp_max)
+        
