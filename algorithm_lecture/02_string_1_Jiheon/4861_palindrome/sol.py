df4062768f7d1d9f7a69fa964a327b167a7ebd32
정지헌 <hun01414@naver.com>
2023-08-07 16:58:26 +0900

    수정

--- algorithm_lecture/02_string_1_Jiheon/4861_palindrome/sol.py
+++ algorithm_lecture/02_string_1_Jiheon/4861_palindrome/sol.py
@@ -1,21 +1,26 @@
 import sys
 sys.stdin = open('input.txt')
 
+def palindrome(str):
+    mid = len(str) // 2
+    for i in range(mid):
+        if str[i] != str[-i-1]:
+            return 0
+    return 1
+
 T = int(input())
 for t in range(T):
-    def palindrome():
-        N, M = map(int, input().split())
-        str = [input() for _ in range(N)]
-        col_str = list(zip(*str))
-        for i in range(N):
-            for j in range(N-M+1):
-                if str[i][j:j+M] == str[i][j:j+M][::-1]:
-                    print(f'#{t+1}', ''.join(str[i][j:j+M]))
-                    return        
-                if col_str[i][j:j+M] == col_str[i][j:j+M][::-1]:
-                    print(f'#{t+1}', ''.join(col_str[i][j:j+M]))
-                    return
+    N, M = map(int, input().split())
+    str = [input() for _ in range(N)]
+    col_str = list(zip(*str))
+    for i in range(N):
+        for j in range(N-M+1):
+            if palindrome(str[i][j:j+M]) == 1:
+                print(f'#{t+1}', ''.join(str[i][j:j+M]))
+                break
+            if palindrome(col_str[i][j:j+M]) == 1:
+                print(f'#{t+1}', ''.join(col_str[i][j:j+M]))
+                break
 
-
-    palindrome()
-
+            
+    
