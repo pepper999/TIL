43572fd2166ea747ffe24633ebc75937263f11c2
정지헌 <hun01414@naver.com>
2023-08-09 17:04:11 +0900

    snila

---
+++ algorithm_lecture/01_List_4_Jiheon/snail/sol.py
@@ -0,0 +1,31 @@
+def snail(num):
+    matrix = [[0]*num for i in range(num)]
+    x , y = 0, 0
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    i = 1
+    while i < num**2:
+        matrix[x][y] = i
+        for j in range(4):
+            try:
+                while matrix[x+dx[j]][y+dy[j]] == 0 and i <= num**2:
+                    x += dx[j]
+                    y += dy[j]
+                    matrix[x][y] = i+1
+                    i += 1
+            except IndexError:
+                continue
+    return matrix
+
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    if N == 1:
+        snail_mat = [[1]]
+    else:
+        snail_mat = snail(N)
+
+    print(f'#{t+1}')
+    for i in range(N):
+        print(*snail_mat[i])
