622f0bce303638c282fdd6d28042f84c42cf92b7
정지헌 <hun01414@naver.com>
2023-08-22 14:22:38 +0900

    트리사칙연산

---
+++ algorithm_lecture/05_tree_Jiheon/1231_inorder/sol.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('input.txt')
+
+# 간단한 중위순회 함수 작성
+def inorder(now):
+    if now <= N :
+        inorder(now*2)
+        print(node[now], end = '')
+        inorder(now*2 + 1)
+
+for t in range(10):
+    N = int(input())
+    # 그래프 순서대로 작성
+    node = [0] * (N+1)
+    # 노드를 각각 연결
+    for i in range(N):
+        temp = list(input().split())
+        node[i+1] = temp[1]
+
+    print(f'#{t+1}', end = ' ')
+    # 중위순회 돌림
+    inorder(1)
+    print()
+
+
+
---
+++ algorithm_lecture/05_tree_Jiheon/1232_fourcalcul/sol.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('input.txt')
+
+def postorder(now):
+    if now:
+        postorder(left[now])
+        postorder(right[now])
+        if node[now] == '+':
+            node[now] = int(node[left[now]]) + int(node[right[now]])
+        elif node[now] == '-':
+            node[now] = int(node[left[now]]) - int(node[right[now]])
+        elif node[now] == '*':
+            node[now] = int(node[left[now]]) * int(node[right[now]])
+        elif node[now] == '/':
+            node[now] = int(node[left[now]]) // int(node[right[now]])
+
+for t in range(10):
+    N = int(input())
+    node = [0] * (N + 1)
+    left = [0] * (N + 1)
+    right = [0] * (N + 1)
+
+    for i in range(N):
+        temp_input = input().split()
+        node[int(temp_input[0])] = temp_input[1]
+        if len(temp_input) == 4:
+            left[int(temp_input[0])] = int(temp_input[2])
+            right[int(temp_input[0])] = int(temp_input[3])
+    postorder(1)
+    print(f'#{t+1}', node[1])
